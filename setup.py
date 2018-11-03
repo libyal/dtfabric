6aabeefdc4bdcbbf58ceb64bd8a74ffcb136a579
Joachim Metz <joachim.metz@gmail.com>
2018-11-03 15:08:35 +0100

    Changes to setup.py to support Fedora Core 29

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ else:
         spec_file = bdist_rpm._make_spec_file(self)
 
       if sys.version_info[0] < 3:
-        python_package = 'python'
+        python_package = 'python2'
       else:
         python_package = 'python3'
 
@@ -95,28 +95,58 @@ else:
           summary = line
 
         elif line.startswith('BuildRequires: '):
-          line = 'BuildRequires: {0:s}-setuptools'.format(python_package)
+          line = 'BuildRequires: {0:s}-setuptools, {0:s}-devel'.format(
+              python_package)
 
         elif line.startswith('Requires: '):
           if python_package == 'python3':
-            line = line.replace('python', 'python3')
+            line = line.replace('python-', 'python3-')
+            line = line.replace('python2-', 'python3-')
 
         elif line.startswith('%description'):
           in_description = True
 
+        elif line.startswith('python setup.py build'):
+          if python_package == 'python3':
+            line = '%py3_build'
+          else:
+            line = '%py2_build'
+
+        elif line.startswith('python setup.py install'):
+          if python_package == 'python3':
+            line = '%py3_install'
+          else:
+            line = '%py2_install'
+
         elif line.startswith('%files'):
-          # Cannot use %{_libdir} here since it can expand to "lib64".
           lines = [
               '%files -n {0:s}-%{{name}}'.format(python_package),
               '%defattr(644,root,root,755)',
-              '%doc ACKNOWLEDGEMENTS AUTHORS LICENSE README',
-              '%{_prefix}/lib/python*/site-packages/**/*.py',
-              '%{_prefix}/lib/python*/site-packages/dtfabric*.egg-info/*',
-              '',
-              '%exclude %{_prefix}/share/doc/*',
-              '%exclude %{_prefix}/lib/python*/site-packages/**/*.pyc',
-              '%exclude %{_prefix}/lib/python*/site-packages/**/*.pyo',
-              '%exclude %{_prefix}/lib/python*/site-packages/**/__pycache__/*']
+              '%doc ACKNOWLEDGEMENTS AUTHORS LICENSE README']
+
+          if python_package == 'python3':
+            lines.extend([
+                '%{python3_sitelib}/dtfabric/*.py',
+                '%{python3_sitelib}/dtfabric/*/*.py',
+                '%{python3_sitelib}/dtfabric*.egg-info/*',
+                '',
+                '%exclude %{_bindir}/*',
+                '%exclude %{_prefix}/share/doc/*',
+                '%exclude %{python3_sitelib}/dtfabric/__pycache__/*',
+                '%exclude %{python3_sitelib}/dtfabric/*/__pycache__/*'])
+
+          else:
+            lines.extend([
+                '%{python2_sitelib}/dtfabric/*.py',
+                '%{python2_sitelib}/dtfabric/*/*.py',
+                '%{python2_sitelib}/dtfabric*.egg-info/*',
+                '',
+                '%exclude %{_bindir}/*',
+                '%exclude %{_prefix}/share/doc/*',
+                '%exclude %{python2_sitelib}/dtfabric/*.pyc',
+                '%exclude %{python2_sitelib}/dtfabric/*.pyo',
+                '%exclude %{python2_sitelib}/dtfabric/*/*.pyc',
+                '%exclude %{python2_sitelib}/dtfabric/*/*.pyo'])
 
           python_spec_file.extend(lines)
           break
@@ -126,6 +156,12 @@ else:
 
           python_spec_file.append(
               '%package -n {0:s}-%{{name}}'.format(python_package))
+          if python_package == 'python2':
+            python_spec_file.append(
+                'Obsoletes: python-dtfabric < %{version}')
+            python_spec_file.append(
+                'Provides: python-dtfabric = %{version}')
+
           python_spec_file.append('{0:s}'.format(summary))
           python_spec_file.append('')
           python_spec_file.append(
